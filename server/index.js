f736b63f45411823fdaef9632639703a8c80847a
jamongjelly <88yangkh@gmail.com>
2021-12-14 12:10:01 +0900

    nodemon 추가 + 보안키 .gitignore로 숨기기

--- server/index.js
+++ server/index.js
@@ -2,15 +2,35 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded 타입 바디 가져오기
+app.use(express.urlencoded({ extended: true }));
+
+// application/json 타입 바디 가져오기
+app.use(express.json());
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:ko31640@cluster0.6jgl1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! Man!3'));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+
+    return res.status(200).json({
+      success: true
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
